cf4c32061dbfc54ee945eb22d129ca393f417c0d
Nathalia6508 <102549857+Nathalia6508@users.noreply.github.com>
2022-09-01 15:39:07 -0400

    Add files via upload

---
+++ Desafios/Desafio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Desafio1
+{
+	private int valor;
+	private int dobro;
+	private int triplo;
+	private float raiz;
+
+	public int Valor { get => valor; set => valor = value; }
+	public int Dobro { get => dobro; set => dobro = value; }
+	public float Raiz { get => raiz; set => raiz = value; }
+	public int Triplo { get => valor; set => valor = value; }
+
+	Console.Write("Informe o valor: ");
+	valor = ReadLine();
+
+	dobro = valor*2;
+	WriteLine(dobro);
+}
